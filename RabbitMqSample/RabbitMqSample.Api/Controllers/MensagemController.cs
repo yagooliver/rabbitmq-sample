86a64f6afdd0317c6c11c4ac2085951680c7dd37
Yago Oliveira <yago@imagemseguranca.com.br>
2020-06-10 07:10:58 -0300

    First commit

---
+++ RabbitMqSample/RabbitMqSample.Api/Controllers/MensagemController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+
+namespace RabbitMqSample.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MensagemController : ControllerBase
+    {
+        private IConnection connection;
+        private IModel channel;
+
+        [HttpGet]
+        public dynamic Get(string mensagem)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+            };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "TesteRabbitMQ",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+            string message =
+                $"{DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss")} - " +
+                $"Conteúdo da Mensagem: {mensagem}";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "TesteRabbitMQ",
+                                 basicProperties: null,
+                                 body: body);
+
+            return new
+            {
+                Retorno = "mensagem enviada com sucesso!"
+            };
+        }
+    }
+}
---
+++ RabbitMqSample/RabbitMqSample.Client/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMqSample.Client
+{
+    class Program
+    {
+        static IConnection connection;
+        static IModel channel;
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                UserName = "guest",
+                Password = "guest",
+                //Port = 44355
+            };
+            connection = factory.CreateConnection();
+            channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "TesteRabbitMQ",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += Consumer_Received;            
+            channel.BasicConsume(queue: "TesteRabbitMQ",
+                 autoAck: true,
+                 consumer: consumer);
+
+            Console.WriteLine("Aguardando mensagens para processamento");
+            Console.WriteLine("Pressione uma tecla para encerrar...");
+            Console.ReadKey();
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var message = Encoding.UTF8.GetString(e.Body);
+            Console.WriteLine(Environment.NewLine +
+                "[Nova mensagem recebida] " + message);
+        }
+    }
+}
